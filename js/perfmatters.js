f6effc3c72119132589a947d3805b53432edeb01
Damola Salisu <36478196+Dsalz@users.noreply.github.com>
2018-07-29 01:13:53 +0100

    Update perfmatters.js

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -9,6 +9,13 @@ function logCRP() {
   stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
 }
 
+ function(w,g){w['GoogleAnalyticsObject']=g;
+      w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+
+      // Optional TODO: replace with your Google Analytics profile ID.
+      ga('create', 'UA-XXXX-Y');
+      ga('send', 'pageview');
+
 window.addEventListener("load", function(event) {
   logCRP();
 });
